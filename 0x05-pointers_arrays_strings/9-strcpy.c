4f3470c030d65f5bce53dcaf36d06a0d272b71a1
Keisha <keishamtawodzera@gmail.com>
2023-06-26 05:55:24 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,37 +1,25 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an interger
- * @s: The pointer to convert
- * Return: A interger
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
  */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
 
-int _atoi(char *s)
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
 
-{
-int c = 0;
-unsigned int ni = 0;
-int min = 1;
-int isi = 0;
-
-while (s[c])
-{
-if (s[c] == 45)
-{
-min *= -1;
-}
-while (s[c] >= 48 && s[c] <= 57)
-{
-isi = 1;
-ni = (ni * 10) + (s[c] - '0');
-c++;
-}
-if (isi == 1)
-{
-break;
-}
-c++;
-}
-ni *= min;
-return (ni);
-}
